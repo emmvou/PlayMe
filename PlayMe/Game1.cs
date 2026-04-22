7f9541ce3ecb1fd8a53ea2dd7ec0cd1101340351
Charlie Vouriot <45200422+emmvou@users.noreply.github.com>
2026-04-22 18:38:17 +0200

    add basic graphic system

--- PlayMe/Game1.cs
+++ PlayMe/Game1.cs
@@ -2,11 +2,14 @@
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using MonoGameLibrary;
+using MonoGameLibrary.Graphics;
 
 namespace PlayMe;
 
 public class Game1 : Core
 {
+    private Sprite _title_board;
+    private AnimatedSprite _button;
 
     public Game1() : base("Play me", 1280, 720, false)
     {
@@ -22,9 +25,15 @@ public class Game1 : Core
 
     protected override void LoadContent()
     {
-        // TODO: use this.Content to load your game content here
+        // Create the texture atlas from the XML configuration file
+        TextureAtlas atlas = TextureAtlas.FromFile(Content, "images/title-atlas-definition.xml");
 
-        base.LoadContent();
+        // retrieve the slime region from the atlas.
+        _title_board = atlas.CreateSprite("title_board");
+
+        // retrieve the bat region from the atlas.
+        _button = atlas.CreateAnimatedSprite("button-animation");
+        _button.Scale = new Vector2(2.0f, 2.0f);
     }
 
     protected override void Update(GameTime gameTime)
@@ -35,6 +44,9 @@ public class Game1 : Core
 
         // TODO: Add your update logic here
 
+        // Update the bat animated sprite.
+        _button.Update(gameTime);
+
         base.Update(gameTime);
     }
 
@@ -42,7 +54,17 @@ public class Game1 : Core
     {
         GraphicsDevice.Clear(Color.CornflowerBlue);
 
-        // TODO: Add your drawing code here
+         // Begin the sprite batch to prepare for rendering.
+        SpriteBatch.Begin(samplerState: SamplerState.PointClamp);
+
+        // Draw the slime sprite
+        _title_board.Draw(SpriteBatch, Vector2.Zero);
+
+        // Draw the bat sprite
+        _button.Draw(SpriteBatch, new Vector2(_title_board.Width + 10, 0));
+
+        // Always end the sprite batch when finished.
+        SpriteBatch.End();
 
         base.Draw(gameTime);
     }
