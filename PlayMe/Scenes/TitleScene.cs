d22d4d536250d3f2e6e2799439497b0bc6d7eaa9
emmvou <emm.vou@gmail.com>
2026-07-30 17:22:33 +0200

    add title screen background

--- PlayMe/Scenes/TitleScene.cs
+++ PlayMe/Scenes/TitleScene.cs
@@ -39,6 +39,19 @@ public class TitleScene : Scene
     // The origin to set for the press enter text when drawing it.
     private Vector2 _pressEnterOrigin;
 
+    // The texture used for the background pattern.
+    private Texture2D _backgroundPattern;
+
+    // The destination rectangle for the background pattern to fill.
+    private Rectangle _backgroundDestination;
+
+    // The offset to apply when drawing the background pattern so it appears to
+    // be scrolling.
+    private Vector2 _backgroundOffset;
+
+    // The speed that the background pattern scrolls.
+    private float _scrollSpeed = 50.0f;
+
     public override void Initialize()
     {
         // LoadContent is called during base.Initialize().
@@ -62,6 +75,13 @@ public class TitleScene : Scene
         size = _font.MeasureString(PRESS_ENTER_TEXT);
         _pressEnterPos = new Vector2(640, 620);
         _pressEnterOrigin = size * 0.5f;
+
+        // Initialize the offset of the background pattern at zero.
+        _backgroundOffset = Vector2.Zero;
+
+        // Set the background pattern destination rectangle to fill the entire
+        // screen background.
+        _backgroundDestination = Core.GraphicsDevice.PresentationParameters.Bounds;
     }
 
     public override void LoadContent()
@@ -81,6 +101,9 @@ public class TitleScene : Scene
         // retrieve the bat region from the atlas.
         _button = atlas.CreateAnimatedSprite("button-animation");
         _button.Scale = new Vector2(0.5f, 0.5f);
+
+        // Load the background pattern texture.
+        _backgroundPattern = Content.Load<Texture2D>("images/background-pattern");
     }
 
     public override void Update(GameTime gameTime)
@@ -93,11 +116,27 @@ public class TitleScene : Scene
 
         // Update the button animated sprite.
         _button.Update(gameTime);
+
+        // Update the offsets for the background pattern wrapping so that it
+        // scrolls down and to the right.
+        float offset = _scrollSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+        _backgroundOffset.X -= offset;
+        _backgroundOffset.Y -= offset;
+
+        // Ensure that the offsets do not go beyond the texture bounds so it is
+        // a seamless wrap.
+        _backgroundOffset.X %= _backgroundPattern.Width;
+        _backgroundOffset.Y %= _backgroundPattern.Height;
     }
 
     public override void Draw(GameTime gameTime)
     {
         Core.GraphicsDevice.Clear(new Color(32, 40, 78, 255));
+
+        // Draw the background pattern first using the PointWrap sampler state.
+        Core.SpriteBatch.Begin(samplerState: SamplerState.PointWrap);
+        Core.SpriteBatch.Draw(_backgroundPattern, _backgroundDestination, new Rectangle(_backgroundOffset.ToPoint(), _backgroundDestination.Size), Color.White * 0.5f);
+        Core.SpriteBatch.End();
 
         // Begin the sprite batch to prepare for rendering.
         Core.SpriteBatch.Begin(samplerState: SamplerState.PointClamp);
